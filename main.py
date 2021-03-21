3e24b6ea0d54095d4f29a6a1f699050e0a8e0367
demogest <demogest@outlook.com>
2021-03-21 21:33:38 +0800

    delete the comment

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def getPow(a, n):
 
 
 def getPi(n):
-    # 使用BBP公式，n即是π的精度，故精度为22000位
     result = (Decimal(4) / Decimal(8 * n + 1) - Decimal(2) / Decimal(8 * n + 4) - Decimal(1) / Decimal(
         8 * n + 5) - Decimal(1) / Decimal(8 * n + 6)) / Decimal(getPow(16, n))
     return result
@@ -25,7 +24,6 @@ if __name__ == '__main__':
     start = time.time()
     params = list(tqdm([i for i in range(int(n))], desc="Creating params list"))
     getcontext().prec = int(n)
-    # 进程池，最大工作数设置为4
     pool = ProcessPoolExecutor(max_workers=4)
     print("start")
     results = list(tqdm(pool.map(getPi,params), total=len(params), desc='Calculating pi with bbp：'))
